97e0c9b92415cc3f87ca8c77ae83f587d6c48fd9
KiiiRifki_14 <muhammadrifki08t@gmail.com>
2026-08-27 13:03:00 +0700

    chore: add push_to_github helper script

---
+++ push_to_github.bat
@@ -0,0 +1,13 @@
+@echo off
+if "%~1"=="" (
+    echo Harap sertakan URL Repository GitHub Anda!
+    echo Contoh penggunaan: push_to_github.bat https://github.com/KiiiRifki14/video-downloader.git
+    exit /b 1
+)
+
+echo Mengatur remote URL ke %~1 ...
+git remote set-url origin %~1
+echo Memulai Push kode ke GitHub ...
+git push -u origin main
+echo.
+echo Selesai! Kode berhasil di-upload ke GitHub.
